b2779322f6b3bfd14c6b99f7d8cb25830c8ab0c8
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-28 11:34:15 +0100

    docs: main components, app organization on comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ function App() {
 
   return (
     <>
+      {/* ORGANIZZAZIONE APP:
+      HOME: pagina benvenuto?
+      ABOUT: pagina contattare?
+      FILMLIST: mostra una serie di film grazie a chiamata ajax (fatta nel context)
+      SEARCHBAR: sottocomponente che sarà usato nell'header
+      FILMCARD: card che sarà mostrata al click sul film, pagina */}
     </>
   )
 }
