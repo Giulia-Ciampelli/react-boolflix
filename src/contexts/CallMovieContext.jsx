d8e3872e5738380aa04808f8acbd8ceca0d3d96c
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-28 14:35:48 +0100

    docs: start CallMovieContext

--- src/contexts/CallMovieContext.jsx
+++ src/contexts/CallMovieContext.jsx
@@ -1,11 +1,17 @@
 // qui andrà la chiamata ajax, stessa cosa fatta ieri notte?
 
 // importa createcontext, usestate e useeffect
+import { createContext, useState, useEffect } from "react";
 
 // crea un context
+const CallMovieContext = createContext();
 
-// crea il componente provider
+// crea ed esporta il componente provider
+export const CallMovieProvider = ({children}) => {
 
-// crea funzione fetch
+    // crea funzione fetch
 
-// ritorna il provider con cui incapsulare l'app (o i com0ponenti dell'app)
+    // ritorna il provider con cui incapsulare l'app (o i componenti dell'app)
+}
+
+export default CallMovieContext;
